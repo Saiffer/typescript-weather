82eb89d60f77871aba1a42714e23429e4cc94a0a
David Aznaurov <aznaurovdavid@yahoo.com>
2021-09-06 21:53:10 -0400

    Entire app

---
+++ src/components/Alert.tsx
@@ -0,0 +1,23 @@
+interface AlertProps {
+    message: string,
+    onClose: () => void
+}
+
+const Alert:React.FC<AlertProps> = ({message, onClose}) => {
+    console.log(message);
+    return (
+        <div className="modal is-active has-text-centered">
+            <div className="moal-background" onClick={onClose}></div>
+            <div className="modal-card">
+                <header className="modal-card-head has-background-danger">
+                    <p className="modal-card-title has-text-white">{message}</p>
+                </header>
+                <footer className="modal-card-foot" style={{justifyContent: 'center'}}>
+                    <button className="button" onClick={onClose}>Close</button>
+                </footer>
+            </div>
+        </div>
+    )
+}
+
+export default Alert
